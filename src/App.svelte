b3a3b2bc31ed9bc5aa54fafb0ef3ae840ae39e17
Mohammmad Kamran Shekh <kshekh@gmail.com>
2023-01-25 23:24:37 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  import Login from "./views/Login.svelte";
+</script>
+
+<main>
+  <Login/>
+
+<!-- 
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1 class="text-red-500 font-bold kamran mt-[20px]">Vite + Svelte</h1>
+
+  <div class="card bg-radial-purple-dark">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p> -->
+</main>
+
+ 

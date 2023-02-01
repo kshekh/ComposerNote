462ba04ca3e912ca080ad8efbb6d0d9bb9a6da7c
Mohammad Naim <naim13797@gmail.com>
2023-02-01 11:26:36 +0530

    i have work on these files

---
+++ src/views/MainMenu.svelte
@@ -0,0 +1,122 @@
+<div class="max-w-sm min-w-screens min-h-screen mx-auto p-4 bg-dark-100 relative"> 
+    <div class="space-y-7">
+        <!-- top section -->
+        <div class="flex items-center justify-between">
+            <div class="flex-col cursor-pointer"><img src="src/assets/images/menu.svg" alt="menu icon"></div>
+            <div class="flex-col cursor-pointer"><h1 class="text-xl font-medium text-white leading-none">Norewing</h1></div>
+            <div class="flex-col inline-flex justify-center items-center cursor-pointer w-8 h-8 rounded-full bg-dark-200 "><img src="src/assets/images/search.svg" alt="menu icon"></div>
+        </div>
+        <!-- middle section -->
+        <div class="divide-y divide-dark-300">
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-1.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">pop music</h2>
+                        <p class="text-base font-light leading-none text-white-200">Arash</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div>
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-2.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">peaceful piano music</h2>
+                        <p class="text-base font-light leading-none text-white-200">relaxing piano music</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div> 
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-3.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">Instrumental Guitar</h2>
+                        <p class="text-base font-light leading-none text-white-200">piano music</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div>  
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-4.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">InsAfter School</h2>
+                        <p class="text-base font-light leading-none text-white-200">Feather</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div> 
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-5.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">Canpeaceful piano music</h2>
+                        <p class="text-base font-light leading-none text-white-200">Love Arash</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div> 
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-6.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">Fragments</h2>
+                        <p class="text-base font-light leading-none text-white-200">Ed Sheeran</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div> 
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-7.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">Party Music</h2>
+                        <p class="text-base font-light leading-none text-white-200">DJ Snake</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div> 
+            <div class="relative flex justify-between py-6">
+                <div class="flex gap-5">
+                    <div class="relative">
+                        <img src="src/assets/images/music-8.png" alt="images">
+                    </div>
+                    <div class="space-y-2">
+                        <h2 class="text-xl font-semibold text-white-300 leading-none">Meditation Music</h2>
+                        <p class="text-base font-light leading-none text-white-200">relaxing music</p>
+                    </div>
+                </div>
+                <div class="flex"><img src="src/assets/images/dots.svg" alt="images"></div>
+            </div>    
+        </div>
+    </div>
+    <!-- Bottom section here -->
+    <div class="bg-gradient-to-r from-dark-300 via-dark-300 to-dark-300 rounded-t-2xl absolute w-full py-3 inset-x-0 bottom-0 flex justify-center items-center">
+        <button class="w-10 h-10 rounded-full bg-red-100  flex justify-center items-center">
+            <span class="w-3 h-3 bg-white rounded-full "></span>
+        </button>  
+    </div>
+</div>
+
+<script>
+   
+  
+</script>
+
+
---
+++ src/views/TempoSelection.svelte
@@ -0,0 +1,42 @@
+<div class="max-w-sm min-w-screens min-h-screen mx-auto p-4 bg-dark-100 relative"> 
+    <div class="space-y-7">
+        <!-- top section -->
+        <div class="flex items-center justify-between">
+            <div class="flex-col cursor-pointer"><img src="src/assets/images/arrow-left.svg" alt="menu icon"></div>
+            <div class="flex-col cursor-pointer mx-auto"><h1 class="text-xl font-medium text-white leading-none">Score Settings</h1></div>
+        </div>
+        <!-- middle section -->
+        <div class="space-y-6">
+              <p class="text-base font-medium leading-sung text-white-300 text-center px-4">Please choose a title and subtitle.</p>
+              <div class="bg-white rounded-md w-20 flex flex-wrap justify-center items-center mx-auto py-4 text-center">
+                <div class="space-y-4 overflow-y-auto max-h-96 w-20">  
+                <span class="text-lg font-medium text-dark-200 block w-full">2/2</span>
+                <span class="text-lg font-medium text-dark-200 block w-full">3/2</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">4/2</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">2/2</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">3/2</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">4/4</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">6/4</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">8/4</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">2/8</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">3/8</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">4/8</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">5/8</span>    
+                <span class="text-lg font-medium text-dark-200 block w-full">6/8</span> 
+                </div>   
+              </div>
+        </div>
+           <!-- Bottom section here -->
+     <div class="text-center font-xs font-normal text-white-200 leading-none flex justify-center">
+        <button class=" bg-white-300 w-full py-3 px-4 rounded-sm text-center text-xl leading-none font-medium cursor-pointer text-black transition ease-in-out hover:bg-white">Accept Rhythm</button>
+    </div>
+    </div>
+
+</div>
+
+<script>
+   
+  
+</script>
+
+
